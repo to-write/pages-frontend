f832159ce7a68611414293eed56c5eef023c829e
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-04 17:37:47 +0900

    Add : Default Layout 디자인 설정 (shared) [feature-#2]

--- shared/components/Header/index.tsx
+++ shared/components/Header/index.tsx
@@ -1,5 +1,10 @@
+import classNames from 'classnames/bind'
+import styles from './index.module.scss'
+
+const cx = classNames.bind(styles)
+
 const Header = () => {
-  return <header>header 내용</header>
+  return <header className={cx('header-container')}>header 내용</header>
 }
 
 export default Header
