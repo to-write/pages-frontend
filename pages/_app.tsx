b9363309dfcedc7439fda7544823d7d070901c45
HyeongSeok <gudtjr3437@naver.com>
2022-12-04 11:04:15 +0900

    Add : Layout component 추가 (shared) [feature-#2]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,11 @@
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
-import '../styles/index.scss'
-
-declare global {
-  interface Window {
-    Kakao: any
-  }
-}
+import { Layout } from '../shared/components'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const kakaoInit = () => {
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY)
-    console.log(window.Kakao.isInitialized()) // true
-  }
-
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
-      <Script src='https://developers.kakao.com/sdk/js/kakao.js' onLoad={kakaoInit} />
-    </>
+    </Layout>
   )
 }
 
