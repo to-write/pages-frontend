8177fad48d915f403fed0e3e30d5d8c5038cd656
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-04 16:23:19 +0900

    Add : 각종 패키지 설치 & env 설정 (shared) [feature-#2]

--- shared/layout/DefaultLayout/index.tsx
+++ shared/layout/DefaultLayout/index.tsx
@@ -1,7 +1,10 @@
 import classNames from 'classnames/bind'
 import styles from './index.module.scss'
 import { ReactNode } from 'react'
+<<<<<<< HEAD
 import { Footer, Header } from '../../components'
+=======
+>>>>>>> bdf0f38 (Add : 각종 패키지 설치 & env 설정 (shared))
 
 const cx = classNames.bind(styles)
 
@@ -11,10 +14,16 @@ interface DefaultLayoutProps {
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
+<<<<<<< HEAD
     <div className={cx('layout-container')}>
       <Header />
       <main className='layout-container__main'>{children}</main>
       <Footer />
+=======
+    <div>
+      <header>헤더 영역</header>
+      <main>{children}</main>
+>>>>>>> bdf0f38 (Add : 각종 패키지 설치 & env 설정 (shared))
     </div>
   )
 }
