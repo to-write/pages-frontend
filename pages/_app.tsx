bdf0f3811d94ed5488db8f547550969f3f1ae9aa
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-04 16:23:19 +0900

    Add : 각종 패키지 설치 & env 설정 (shared) [feature-#2]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,55 @@
 import '../styles/index.scss'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
-import { Layout } from '../shared/components'
+import { FunctionComponent, useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { DefaultLayout } from '../shared/layout'
 
-const App = ({ Component, pageProps }: AppProps) => {
+export type AppPropsWithLayout<P = Record<string, unknown>> = AppProps<P> & {
+  Component: {
+    Layout: FunctionComponent
+    // FIXME: 추후 LayoutProps 정해지면 수정
+    LayoutProps: any
+  }
+  // FIXME: 추후 Session store 정해지면 수정
+  session?: any
+}
+
+const App = ({ Component, pageProps, session }: AppPropsWithLayout) => {
+  const Layout = Component.Layout ?? DefaultLayout
+  const LayoutProps = Component.LayoutProps ?? {}
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name='referrer' content='unsafe-url' />
+        <title>{LayoutProps?.metaTitle || '프로젝트 이름'}</title>
+      </Head>
+      <div className='app'>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Layout {...LayoutProps}>
+              <Component {...pageProps} />
+            </Layout>
+            <div id='root-modal' />
+          </Hydrate>
+          <ReactQueryDevtools initialIsOpen={false} />
+          {/* {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />} */}
+        </QueryClientProvider>
+      </div>
+    </>
   )
 }
 
