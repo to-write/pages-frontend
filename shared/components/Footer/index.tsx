2dab38ff585c2d5141e4c39f5f6c2f5e94f225b1
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-04 17:37:47 +0900

    Add : Default Layout 디자인 설정 (shared) [feature-#2]

--- shared/components/Footer/index.tsx
+++ shared/components/Footer/index.tsx
@@ -1,5 +1,10 @@
+import classNames from 'classnames/bind'
+import styles from './index.module.scss'
+
+const cx = classNames.bind(styles)
+
 const Footer = () => {
-  return <footer>footer 내용</footer>
+  return <footer className={cx('footer-container')}>footer 내용</footer>
 }
 
 export default Footer
