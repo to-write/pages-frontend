1536a1c00f3e789ba141afc4993d6bd81a99fac4
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-11 17:20:56 +0900

    Add : PC 레이아웃 변경 & 세부 디자일 조정 (shared) [feature-#2]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import AsidePortal from '../shared/components/AsidePortal'
+
 const HomePage = () => {
-  return <div>랜딩 페이지</div>
+  return (
+    <div>
+      랜딩 페이지
+      <AsidePortal>
+        <aside>어사이드입니다</aside>
+      </AsidePortal>
+    </div>
+  )
 }
 
 HomePage.LayoutProps = {
   metaTitle: '랜딩페이지',
+  aside: true,
 }
 
 export default HomePage
--- shared/layout/DefaultLayout/index.tsx
+++ shared/layout/DefaultLayout/index.tsx
@@ -1,29 +1,20 @@
 import classNames from 'classnames/bind'
 import styles from './index.module.scss'
-import { ReactNode } from 'react'
-<<<<<<< HEAD
 import { Footer, Header } from '../../components'
-=======
->>>>>>> bdf0f38 (Add : 각종 패키지 설치 & env 설정 (shared))
+import { DefaultLayoutProps } from '../../types'
+import { ReactNode } from 'react'
 
 const cx = classNames.bind(styles)
 
-interface DefaultLayoutProps {
-  children: ReactNode
-}
-
-const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+const DefaultLayout = ({ children, aside = false }: DefaultLayoutProps) => {
   return (
-<<<<<<< HEAD
     <div className={cx('layout-container')}>
       <Header />
-      <main className='layout-container__main'>{children}</main>
+      <div className='layout-container__main-conatiner'>
+        {aside && <div id='layoutAside' className='layout-container__aisde' />}
+        <main className='layout-container__main'>{children}</main>
+      </div>
       <Footer />
-=======
-    <div>
-      <header>헤더 영역</header>
-      <main>{children}</main>
->>>>>>> bdf0f38 (Add : 각종 패키지 설치 & env 설정 (shared))
     </div>
   )
 }
