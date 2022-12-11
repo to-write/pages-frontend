8816dc90565e40bb6ed95a2ca21afdb3066f2179
김형석 <gudtjr3437@jobkorea.co.kr>
2022-12-11 17:19:59 +0900

    Add : AsidePortal 컴포넌트 작성 (shared) [feature-#2]

---
+++ shared/components/AsidePortal/index.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames/bind'
+import { ReactNode, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { useMount } from 'react-use'
+import styles from './index.module.scss'
+
+const cx = classNames.bind(styles)
+
+export interface AsidePortalProps {
+  children: ReactNode
+}
+
+const AsidePortal = ({ children }: AsidePortalProps) => {
+  const asideRef = useRef<HTMLElement | null>()
+  const [mount, setMount] = useState(false)
+
+  useMount(() => {
+    setMount(true)
+    document && (asideRef.current = document.getElementById('layoutAside'))
+  })
+
+  if (mount && asideRef.current) {
+    return createPortal(children, asideRef.current)
+  }
+  return null
+}
+
+export default AsidePortal
