9f176b8ee91ec70cf4f401dc712c80b206371c74
Sangji <sjlee@beyless.com>
2022-12-13 17:36:47 +0900

    기타 수정 사항

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,47 +1,25 @@
-import styles from './login.module.scss'
 import classNames from 'classnames/bind'
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
+import styles from './index.module.scss'
+import GoogleButton from '../../components/oauth/google'
+import KakaoButton from '../../components/oauth/kakao'
 
 const cx = classNames.bind(styles)
 
-const Login = () => {
-  // const { data: session } = useSession();
-  // 등록한 redirectUri를 매개변수로 넣어준다.
-
-  const kakaoLogin = () => {
-    window.Kakao.Auth.authorize({
-      redirectUri: 'http://localhost:3000/kakao',
-    })
-  }
-
-  const googleLogin = () => {}
-
+const LoginPage = () => {
   return (
-    <div className={styles.loginBackground}>
-      <div className={styles.loginContainer}>
-        <div className={styles.contentBox}>
-          <h1>로고 영역</h1>
-        </div>
-        <div className={styles.buttonWrapper}>
-          <button type='button' onClick={kakaoLogin}>
-            카카오 로그인
-          </button>
-
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse)
-            }}
-            onError={() => {
-              console.log('Login Failed')
-            }}
-          />
-        </div>
+    <div className={cx('login-container')}>
+      <div className='login-container__logo'>
+        <h1>로고 영역</h1>
+      </div>
+      <div className='login-container__button-container'>
+        <KakaoButton />
+        <GoogleButton />
       </div>
     </div>
   )
 }
 
-export default Login
+export default LoginPage
 
 // http://localhost:3000/kakao?code=1mxdTdVB3UrDy78vZplb5EzKyTCUWyxAXbD4Id6iUbzoHJvNcvb2nt6q_A1GGAHa3AztOgopdSkAAAGEwvdbvA
 // 토큰 저장1
