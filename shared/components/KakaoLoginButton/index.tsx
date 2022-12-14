ac549b41b00bff2dd5f0741fb92d2c57e8b03f2c
saanglee <sangllee20@gmail.com>
2022-12-14 21:03:26 +0900

    Feat: 카카오 로그인 컴포넌트 생성 (shared)

---
+++ shared/components/KakaoLoginButton/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './index.module.scss'
+import Link from 'next/link'
+
+const cx = classNames.bind(styles)
+
+type KakaoButtonProps = {}
+
+// TODO Kakao JS SDK Login
+// const kakaoLogin = () => {
+//   window.Kakao.Auth.authorize({
+//     redirectUri: 'http://localhost:3000/kakao',
+//   })
+// }
+
+const KakaoButton: React.FC<KakaoButtonProps> = () => {
+  return (
+    <Link href='http://localhost:8080/oauth2/authorization/kakao'>
+      <button className={cx('kakao-button')} type='button'>
+        카카오 로그인
+      </button>
+    </Link>
+  )
+}
+export default KakaoButton
