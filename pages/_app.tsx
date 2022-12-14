fb1386618c05b46463d40b8482f93d7a831bc742
saanglee <sangllee20@gmail.com>
2022-12-14 21:01:24 +0900

    Comment: 카카오 로그인에 필요한 함수 임시 주석 처리 (shared)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,9 +16,9 @@ export type AppPropsWithLayout<P = Record<string, unknown>> = AppProps<P> & {
   // FIXME: 추후 Session store 정해지면 수정
   session?: any
 }
-
+// TODO Kakao JS SDK Login
 // declare global {
-//   // Kakao 함수를 전역에서 사용할 수 있도록 선언
+//   Kakao 함수를 전역에서 사용할 수 있도록 선언
 //   interface Window {
 //     Kakao: any
 //   }
@@ -38,13 +38,12 @@ const App = ({ Component, pageProps, session }: AppPropsWithLayout) => {
         },
       })
   )
-
+  // TODO Kakao JS SDK Login
   // function kakaoInit() {
-  //   // 페이지가 로드되면 실행
   //   window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY)
   //   console.log(window.Kakao.isInitialized())
   // }
-  // TODO GoogleOAuthProvider 위치 맞는 지 확인
+
   return (
     <>
       <Head>
@@ -67,18 +66,5 @@ const App = ({ Component, pageProps, session }: AppPropsWithLayout) => {
     </>
   )
 }
-// const App = ({ Component, pageProps: { ...pageProps } }: AppProps) => {
-//   const kakaoInit = () => {
-//     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY)
-//   }
 
-//   return (
-//     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''}>
-//       <Component {...pageProps} />
-//       <Script src='https://developers.kakao.com/sdk/js/kakao.js' onLoad={kakaoInit} />
-//     </GoogleOAuthProvider>
-//   )
-// }
-// <SessionProvider /> also takes care of keeping the session updated and synced between browser tabs and windows.
-// By wrapping our component in a Session``Provider, we enable session state to be shared between pages.
 export default App
