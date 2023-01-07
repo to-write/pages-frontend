ac32e3b621a6d00216af8f122dac81a74e4f8893
김형석 <gudtjr3437@jobkorea.co.kr>
2023-01-07 15:28:11 +0900

    Add : Google Login Button Component  (shared) [feature-#3]

---
+++ pages/test/index.tsx
@@ -0,0 +1,9 @@
+import GoogleLoginButton from '../../shared/components/GoogleLoginButton'
+
+const TestPage = () => {
+  return (
+    <div>
+      <GoogleLoginButton />
+    </div>
+  )
+}
--- shared/components/GoogleLoginButton/index.tsx
+++ shared/components/GoogleLoginButton/index.tsx
@@ -1,3 +1,16 @@
-const GoogleLoginButton = () => {}
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
+
+const GoogleLoginButton = () => {
+  const onSuccess = (response: CredentialResponse) => {
+    // FIXME: 여기서 백엔드한테 쏴줌
+    console.log(response)
+  }
+
+  const onFail = () => {
+    alert('로그인 실패!')
+  }
+
+  return <GoogleLogin onSuccess={onSuccess} onError={onFail} />
+}
 
 export default GoogleLoginButton
