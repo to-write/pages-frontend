b0082be5c5ca7ccc593f45c024508e657c756c26
김형석 <gudtjr3437@jobkorea.co.kr>
2023-01-28 16:29:15 +0900

    Add : custom axios api client 모듈 구성 [feature-#4]

---
+++ shared/api-client/instance.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { FailureResponse } from '../types/common'
+import { AuthError, BadRequestError, ForbiddenError, NotFoundError } from './errors'
+
+function createAxios(requestConfig: AxiosRequestConfig): AxiosInstance {
+  const axiosInstance = axios.create({
+    baseURL: requestConfig.baseURL,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  })
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      // eslint-disable-next-line no-console
+      console.error('API call failed', error?.response?.config?.url || error)
+      const status = error?.response?.status
+      const errorData: FailureResponse = error?.response?.data
+
+      // NOTE: 400번대 에러 핸들링
+      switch (status) {
+        case 400:
+          if (typeof window !== 'undefined') {
+            window.dispatchEvent(new CustomEvent('apiError', { detail: errorData }))
+            break
+          } else {
+            throw BadRequestError(errorData)
+          }
+        case 401:
+          throw AuthError(errorData)
+
+        case 403:
+          if (typeof window !== 'undefined') {
+            window.dispatchEvent(new CustomEvent('apiError', { detail: errorData }))
+            break
+          } else {
+            throw ForbiddenError(errorData)
+          }
+
+        case 404:
+          throw NotFoundError()
+      }
+
+      return Promise.reject(errorData || error)
+    }
+  )
+
+  return axiosInstance
+}
+
+export const axiosAPI = createAxios({ baseURL: 'NEXT_PUBLIC_API_URL' })
--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,2 +1,3 @@
 export * from './components'
 export * from './SocialLogin'
+export * from './common'
