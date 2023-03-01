1ac6fa728ea62de82ee84029d4904268c36e022d
saanglee <sangllee20@gmail.com>
2023-03-01 19:18:40 +0900

    feature: Button Component

--- shared/components/Button/index.tsx
+++ shared/components/Button/index.tsx
@@ -1,21 +1,24 @@
-import React, { ReactNode } from 'react'
+import React, { MouseEventHandler, ReactNode } from 'react'
 import classNames from 'classnames/bind'
 import styles from './index.module.scss'
 
 const cx = classNames.bind(styles)
-// size: 56 50 44 40 32 28
 
 interface ButtonProps {
-  type?: 'cta' | 'primary' | 'secondary'
-  size?: 'x-large' | 'large' | 'medium' | 'small' | 'x-small'
-  isLoading?: boolean
   children: ReactNode
+  type: 'cta' | 'primary' | 'secondary' | 'disabled'
+  size: 'huge' | 'x-large' | 'large' | 'medium' | 'small' | 'x-small' | 'mini'
+  isDisabled?: boolean
+  isLoading?: boolean // TODO: 예정
+  extendClass?: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-const Button = ({ type, size, isLoading = false, children }: ButtonProps) => {
+const Button = ({ type, size, isLoading = false, isDisabled = false, children, extendClass, onClick }: ButtonProps) => {
   return (
-    <button type='button' className={cx(type, size)}>
-      {isLoading ? '로딩 스피너' : children}
+    <button type='button' className={cx('button', type, size, extendClass)} disabled={isDisabled} onClick={onClick}>
+      {/* {isLoading ? '로딩 스피너' : children} TODO: 예정 */}
+      {children}
     </button>
   )
 }
