b597b0c77af857953566efb0ac39237f24766197
saanglee <sangllee20@gmail.com>
2023-04-13 12:53:28 +0900

    Modify

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,7 +2,6 @@ import classNames from 'classnames/bind'
 import { GoogleLoginButton } from '../../shared/components'
 import styles from './index.module.scss'
 import KakaoButton from '../../shared/components/KakaoLoginButton'
-import { Display, Heading, Body } from '../../shared/components/Typography'
 
 const cx = classNames.bind(styles)
 
@@ -13,14 +12,7 @@ const LoginPage = () => {
       <h1 className='login-page__title'>로그인 페이지</h1>
       <div className='login-container__button-container'>
         <KakaoButton />
-        <GoogleButton />
-
-        {/* Typography 시용 예 */}
-        <Display type='44'>Display</Display>
-        <Heading type='15' weight='regular'>
-          Heading
-        </Heading>
-        <Body>Body</Body>
+        {/* <GoogleButton /> */}
       </div>
 
       <GoogleLoginButton type='login' />
