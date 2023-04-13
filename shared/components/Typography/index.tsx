89f36c032c5b0a8034f5ad8abf5b4e3e7e98c16a
saanglee <sangllee20@gmail.com>
2023-04-13 12:39:46 +0900

    Modify: 컴포넌트명 변경, 색상타입 추가

--- shared/components/Typography/index.tsx
+++ shared/components/Typography/index.tsx
@@ -4,57 +4,58 @@ import styles from './index.module.scss'
 
 const cx = classNames.bind(styles)
 
-type DisplayTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
-type BodyTag = 'span' | 'div' | 'em' | 'li' | 'ul' | 'strong'
+type TextDisplayTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+type TextBodyTag = 'span' | 'div' | 'em' | 'li' | 'ul' | 'strong'
 
-type DisplayType = '50' | '44' | '38' | '32' | '28' | '24' | '22' | '22-semibold'
-type BodyType = '16' | '15' | '14' | '13'
+type TextDisplayType = '50' | '44' | '38' | '32' | '28' | '24' | '22' | '22-semibold'
+type TextBodyType = '16' | '15' | '14' | '13'
 
 interface Typography {
-  children: ReactNode
+  children?: React.ReactNode
+  colors?: 'black' | 'primary' | 'secondary' | 'positive' | 'negative'
 }
 
-interface DisplayProps extends Typography {
-  type?: DisplayType
-  tag?: DisplayTag
+interface TextDisplayProps extends Typography {
+  type?: TextDisplayType
+  tag?: TextDisplayTag
 }
 
-interface BodyProps extends Typography {
-  type?: BodyType
-  tag?: BodyTag
+interface TextBodyProps extends Typography {
+  type?: TextBodyType
+  tag?: TextBodyTag
 }
 
-interface HeadingProps extends Typography {
-  type?: '20' | '18' | BodyType
-  tag?: DisplayTag | BodyTag
+interface TextHeadingProps extends Typography {
+  type?: '20' | '18' | TextBodyType
+  tag?: TextDisplayTag | TextBodyTag
   weight?: 'bold' | 'semi-bold' | 'medium' | 'regular'
 }
 
-export const Display = ({ type = '32', tag = 'h1', children }: DisplayProps) => {
+export const TextDisplay = ({ type = '32', tag = 'h1', colors = 'black', children }: TextDisplayProps) => {
   const Tag = `${tag}` as keyof JSX.IntrinsicElements
   const fontSize = useMemo(() => ({ fontSize: `${type}px` }), [])
   return (
-    <Tag className={cx('display', `display-${type}`)} style={fontSize}>
+    <Tag className={cx('display', `display-${type}`, colors)} style={fontSize}>
       {children}
     </Tag>
   )
 }
 
-export const Body = ({ type = '14', tag = 'span', children }: BodyProps) => {
+export const TextBody = ({ type = '14', tag = 'span', colors = 'black', children }: TextBodyProps) => {
   const Tag = `${tag}` as keyof JSX.IntrinsicElements
   const fontSize = useMemo(() => ({ fontSize: `${type}px` }), [])
   return (
-    <Tag className={cx('body', `body-${type}`)} style={fontSize}>
+    <Tag className={cx('body', `body-${type}`, colors)} style={fontSize}>
       {children}
     </Tag>
   )
 }
 
-export const Heading = ({ type = '16', tag = 'h2', children, weight }: HeadingProps) => {
+export const TextHeading = ({ type = '16', tag = 'h2', colors = 'black', children, weight }: TextHeadingProps) => {
   const Tag = `${tag}` as keyof JSX.IntrinsicElements
   const fontSize = useMemo(() => ({ fontSize: `${type}px` }), [])
   return (
-    <Tag className={cx(type, weight)} style={fontSize}>
+    <Tag className={cx(type, weight, colors)} style={fontSize}>
       {children}
     </Tag>
   )
