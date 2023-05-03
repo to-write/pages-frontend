d8a43e45778f133a2c2157b78a079cb165d48bb9
saanglee <sangllee20@gmail.com>
2023-05-03 16:58:12 +0900

    Feature: default layout 수정 진행 중

--- shared/layout/DefaultLayout/index.tsx
+++ shared/layout/DefaultLayout/index.tsx
@@ -2,24 +2,45 @@ import classNames from 'classnames/bind'
 import styles from './index.module.scss'
 import { Footer, Header } from '../../components'
 import { DefaultLayoutProps } from '../../types'
+import Navigation from '../../components/Navigation'
+import Button from '../../components/Button'
 
 const cx = classNames.bind(styles)
 
-const DefaultLayout = ({
-  aside = false,
-  children,
-  menuType = 'BACK',
-  menuName,
-  isMobile = false,
-}: DefaultLayoutProps) => {
+const DefaultLayout = ({ children, aside = false }: DefaultLayoutProps) => {
+  const tempOnClick = () => console.log('클릭')
+
   return (
-    <div className={cx('layout-container')}>
-      <Header menuType={menuType} menuName={menuName} isMobile={isMobile} />
-      <div className='layout-container__main-conatiner'>
-        {!isMobile && aside && <aside id='layout-aside' className='layout-container__aside' />}
-        <main className='layout-container__main'>{children}</main>
+    <div className={cx('layout')}>
+      <Navigation
+        menuItems={[
+          { menuIcon: 'iconA', menuName: 'nameA', onClick: tempOnClick },
+          { menuIcon: 'iconB', menuName: 'nameB', onClick: tempOnClick },
+          { menuIcon: 'iconC', menuName: 'nameC', onClick: tempOnClick },
+        ]}
+      >
+        <Button type='cta' size='large'>
+          icon 문장 쓰기
+        </Button>
+      </Navigation>
+      <div className='layout__main-container'>
+        <Header />
+        <main className='layout__main'>
+          {children}
+          {/* <button type='button' onClick={() => console.log('test')}>
+            click me
+          </button>
+          <br /> */}
+          <button type='button' onClick={tempOnClick}>
+            click me
+          </button>
+        </main>
       </div>
-      <Footer />
+      {/* <div className='layout__main-conatiner'>
+        <Header />
+        {aside && <div id='layoutAside' className='layout__aisde' />}
+        <main className='layout__main'>{children}</main>
+      </div> */}
     </div>
   )
 }
