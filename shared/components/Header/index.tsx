72172d800b53e09c0ede35b78364d95582380860
김형석 <gudtjr3437@naver.com>
2023-05-14 16:58:07 +0900

    Modify : Header 컴포넌트 모바일 메뉴 버튼 로직 추가 [feature-#12]

--- shared/components/Header/index.tsx
+++ shared/components/Header/index.tsx
@@ -1,10 +1,46 @@
 import classNames from 'classnames/bind'
 import styles from './index.module.scss'
+import { DefaultLayoutProps, MenuType } from '../../types'
+import Icon from '../Icon'
+import { useRouter } from 'next/router'
+import { MouseEvent } from 'react'
 
 const cx = classNames.bind(styles)
 
-const Header = () => {
-  return <header className={cx('header-container')}>header 내용</header>
+interface HeaderProps extends Pick<DefaultLayoutProps, 'menuType' | 'menuName' | 'isMobile'> {}
+
+const Header = ({ menuType, menuName, isMobile }: HeaderProps) => {
+  const router = useRouter()
+
+  const handleMenuBtn = (e: MouseEvent<HTMLButtonElement>, type: MenuType) => {
+    if (type === 'BACK') {
+      router.back()
+      return
+    }
+    if (type === 'CLOSE') {
+      router.push('/')
+    }
+  }
+
+  return (
+    <header className={cx('header-container')}>
+      {!isMobile && (
+        <div className='header-container__desktop'>
+          <span>desktop {menuName} 내용</span>
+        </div>
+      )}
+      {isMobile && (
+        <div className='header-container__mobile'>
+          {menuType !== 'NONE' && (
+            <button type='button' onClick={(e) => handleMenuBtn(e, 'BACK')}>
+              <Icon iconName={menuType} size={24} />
+            </button>
+          )}
+          <span>{menuName}</span>
+        </div>
+      )}
+    </header>
+  )
 }
 
 export default Header
