c0d14478b9fda89b9e6fe9f707f4c8c4017f3e48
김형석 <gudtjr3437@naver.com>
2023-05-14 16:56:50 +0900

    Modify : 불필요한 코드 제거 [feature-#12]

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `http://219.248.110.167:30800/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
