9044926168d8788a35b9347f1331d949aa94be2f
김형석 <gudtjr3437@naver.com>
2023-05-28 13:27:24 +0900

    Modify: Typography 컴포넌트 완료

---
+++ shared/components/TextField/index.tsx
@@ -0,0 +1,73 @@
+import classNames from 'classnames/bind'
+import { ChangeEventHandler, MouseEventHandler, useRef, useState } from 'react'
+import Button from '../Button'
+import Typography from '../Typography'
+import UserProfileImg from '../UserProfileImg'
+import styles from './index.module.scss'
+
+const DEFAULT_PLACE_HOLDER = '텍스트를 입력해주세요.'
+const MAX_LENGTH = 300
+
+const cx = classNames.bind(styles)
+
+export interface TextFieldProps {
+  placeholder?: string
+  withProfileImg?: boolean
+  isSubmit?: boolean
+  onSubmit?: (text: string) => void
+  className?: string
+}
+
+const TextField = ({
+  placeholder = DEFAULT_PLACE_HOLDER,
+  withProfileImg,
+  isSubmit = false,
+  onSubmit,
+  className,
+}: TextFieldProps) => {
+  const [inputValue, setInputValue] = useState('')
+  const textRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleChangeTextArea: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleSubmit: MouseEventHandler<HTMLButtonElement> = () => {
+    onSubmit?.(inputValue.trim())
+    setInputValue('')
+  }
+
+  return (
+    <div className={cx('textfield', className)}>
+      {withProfileImg && (
+        <div className='textfield__profile'>
+          <UserProfileImg size='small' />
+        </div>
+      )}
+      <div className='textfield__content'>
+        <textarea
+          className='textfield__input'
+          rows={5}
+          ref={textRef}
+          value={inputValue}
+          onChange={handleChangeTextArea}
+          placeholder={placeholder}
+        />
+
+        {isSubmit && (
+          <Button
+            extendClass='textfield__submit-btn'
+            size='medium'
+            variant='cta'
+            isDisabled={inputValue.trim() === ''}
+            onClick={handleSubmit}
+          >
+            <Typography type='display-22'>등록하기</Typography>
+          </Button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default TextField
