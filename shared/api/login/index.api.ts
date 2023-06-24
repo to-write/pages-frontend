67ec200d7a31f291b2f1974cedcaf322ac529b6b
김형석 <gudtjr3437@naver.com>
2023-06-24 16:14:52 +0900

    Modify : prod에서 base Url 문제 수정 [feature-#16]

--- shared/api/login/index.api.ts
+++ shared/api/login/index.api.ts
@@ -1,8 +1,10 @@
+import axios from 'axios'
 import { axiosAPI } from '../../api-client'
 import { CheckTokenRequest, LoginReissueRequest, LoginRequest, LoginResponse } from '../../types/api'
 
+// FIXME: 로그인은 219.248.110.167:30800 해당 url 로 보내주어야함 추후 수정되면 axiosAPI instance로 변경
 export const snsLogin = async (params: LoginRequest): Promise<LoginResponse> => {
-  const { data } = await axiosAPI.post('/login', params)
+  const { data } = await axios.post('http://219.248.110.167:30800/login', params)
   return data
 }
 
